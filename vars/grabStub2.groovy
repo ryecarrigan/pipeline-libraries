40d4ca09230c490013a9338ec1ba77b60d56f1a1
Rye Carrigan <rcarrigan@cloudbees.com>
2022-03-07 14:32:30 -0500

    test with imports

--- vars/grabStub2.groovy
+++ vars/grabStub2.groovy
@@ -1,8 +1,16 @@
-#!groovy
-@Grab(group='org.apache.httpcomponents', module='httpclient', version='4.2.1')
-@Grab(group='org.apache.httpcomponents', module='httpcore', version='4.2.1')
-@Grab(group='org.codehaus.groovy', module='groovy-json', version='2.3.7')
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
+import groovyx.net.*
+import groovyx.net.http.*
+import groovyx.net.http.RESTClient
+import static groovyx.net.http.ContentType.*
+import groovy.json.JsonSlurper
+import groovy.json.*
+import static groovy.json.JsonOutput.*
+import static groovyx.net.http.Method.*
+import groovy.util.XmlParser
+import org.apache.commons.lang3.*
+import java.io.*
+import java.text.SimpleDateFormat
+import groovyx.net.http.RESTClient
 
 def call() {
   println "Hello"
