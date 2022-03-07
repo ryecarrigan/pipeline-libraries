857f594ee756caaf6c0addad889f450d22df8b1a
Rye Carrigan <rcarrigan@cloudbees.com>
2022-03-07 14:41:31 -0500

    use contenttype import

--- vars/grabStub2.groovy
+++ vars/grabStub2.groovy
@@ -1,19 +1,20 @@
 @Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
 
+import groovy.json.*
+import groovy.json.JsonSlurper
 import groovyx.net.*
 import groovyx.net.http.*
 import groovyx.net.http.RESTClient
-import static groovyx.net.http.ContentType.*
-import groovy.json.JsonSlurper
-import groovy.json.*
-import static groovy.json.JsonOutput.*
-import static groovyx.net.http.Method.*
+import groovyx.net.http.RESTClient
 import groovy.util.XmlParser
 import org.apache.commons.lang3.*
 import java.io.*
 import java.text.SimpleDateFormat
-import groovyx.net.http.RESTClient
+
+import static groovy.json.JsonOutput.*
+import static groovyx.net.http.Method.*
+import static groovyx.net.http.ContentType.*
 
 def call() {
-  println "Hello"
+  println "Hello: ${TEXT}"
 }
