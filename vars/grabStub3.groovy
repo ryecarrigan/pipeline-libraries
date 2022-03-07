22dccf6ab8bfff0420fd6619831b697e05735df4
Rye Carrigan <rcarrigan@cloudbees.com>
2022-03-07 14:42:40 -0500

    add import without grab

--- vars/grabStub3.groovy
+++ vars/grabStub3.groovy
@@ -1,9 +1,3 @@
-#!groovy
-@Grab(group='org.apache.httpcomponents', module='httpclient', version='4.2.1')
-@Grab(group='org.apache.httpcomponents', module='httpcore', version='4.2.1')
-@Grab(group='org.codehaus.groovy', module='groovy-json', version='2.3.7')
-@Grab(group='org.codehaus.groovy.modules.http-builder', module='http-builder', version='0.7.1')
-
 def call() {
-  println "Hello"
+  println "Hello: ${groovyx.net.http.ContentType.TEXT}"
 }
