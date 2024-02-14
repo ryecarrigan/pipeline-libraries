53935442b1dc851bc9a09c7e195721120937bbfb
Rye Carrigan <rcarrigan@cloudbees.com>
2024-02-14 14:07:06 -0500

    Testing read from JSON file (4)

--- vars/doTest.groovy
+++ vars/doTest.groovy
@@ -1,4 +1,5 @@
 def call() {
   pipelineUtil.script = this
-  pipelineUtil.getConfiguration('a', 'b')
+
+  echo pipelineUtil.getConfiguration('testing', 'b')
 }
--- vars/pipelineUtil.groovy
+++ vars/pipelineUtil.groovy
@@ -7,6 +7,8 @@ class pipelineUtil implements Serializable {
   static def getConfiguration(environment, key) {
     def text = script.libraryResource('environment.json')
     def json = script.readJSON text: text
+
+    return json[environment][key]
   }
 
 }
